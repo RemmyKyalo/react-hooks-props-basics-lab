41f2c5faa79e9b55196af10d4d1b3266cbd5abff
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2023-01-11 23:55:30 +0300

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,19 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
-
-// pass this data down as props to the child component(s) that need it!
+import Links from "./Links";
 import user from "../data/user";
-console.log(user);
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home color={user.color} name ={user.name} city ={user.city}/>
+      <About bio={user.bio} />
+      <Links github={user.links.github} linkedin={user.links.linkedin} />
     </div>
   );
 }
 
 export default App;
+
